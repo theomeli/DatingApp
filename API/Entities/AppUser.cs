21eaef2aa6eb887a8a0aaef3bd698201a79677d2
Theodosios Melidis <theodosios.melidis@intrasoft-intl.com>
2022-06-13 23:08:50 +0300

    feat: authorization/authentication

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] Passwordhash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
