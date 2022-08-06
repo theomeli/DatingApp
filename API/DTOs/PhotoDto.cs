c48127877cc901d3a3150a365945fb457c723972
Theodosios Melidis <theodosios.melidis@intrasoft-intl.com>
2022-08-06 19:08:54 +0300

    feat: configuring user functionalities

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs
+{
+  public class PhotoDto
+  {
+    public int Int { get; set; }
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+  }
+}
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 
 namespace API.Extensions
 {
@@ -16,6 +18,8 @@ namespace API.Extensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
       services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
       services.AddDbContext<DataContext>(options =>
       {
         options.UseSqlite(config.GetConnectionString("DefaultConnection"));
